75ee19c917580a9cf2f7535aa271686ed8735926
Reebby <rebbyoshiobughie@gmail.com>
2018-09-19 14:00:17 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,6 @@ shouldComponentUpdate(nextProps, nextState) {
 		return false;
 	}
 render() {
-<<<<<<< HEAD
-=======
-	console.log('Header')
->>>>>>> 9af1d05e8504e449885f79a22972ab18f7e4deeb
 	return <h1 className="f1"> Rebby's Robots </h1>
 }
 }
